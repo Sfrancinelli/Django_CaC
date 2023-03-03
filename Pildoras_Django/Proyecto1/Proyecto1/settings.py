bc6d37013d596dfa9728ab365b723fcc5f38ed72
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-03-03 10:06:29 -0300

    Added html tags for url saludo/

--- Pildoras_Django/Proyecto1/Proyecto1/settings.py
+++ Pildoras_Django/Proyecto1/Proyecto1/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import security_key
+from Proyecto1 import security_key
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- Pildoras_Django/Proyecto1/Proyecto1/views.py
+++ Pildoras_Django/Proyecto1/Proyecto1/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 # Every function created in the views file it's a view for our web.
 def saludo(request):
 
-    return HttpResponse("Hola mundo!")
+    documento = "<html><body><h1>Hola Mundo!</h1></body></html>"
+
+    return HttpResponse(documento)
 
 def goodbye(request):
 
