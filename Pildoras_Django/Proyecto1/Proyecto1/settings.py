bb79d30f64dd92835f68bb218ef699e756683acf
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-03-03 09:41:58 -0300

    Added git ignore and updated settings!

--- Pildoras_Django/Proyecto1/Proyecto1/settings.py
+++ Pildoras_Django/Proyecto1/Proyecto1/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-
+import security_key
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u&lqb=r9x^e(!83@&v0j!(z@904*conxy)n4390m-5zmx1@ldm'
+SECRET_KEY = security_key.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
