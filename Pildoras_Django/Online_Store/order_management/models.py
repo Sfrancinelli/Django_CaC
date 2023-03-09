14f0ad360ec91ca007cda81cdf20fad12f4004b7
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-03-09 15:43:03 -0300

    Creating first BBDD

--- Pildoras_Django/Online_Store/order_management/models.py
+++ Pildoras_Django/Online_Store/order_management/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Clients(models.Model):
-    name = models.CharField(max_lenght=30)
-    address = models.CharField(max_lenght=80)
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=80)
     email = models.EmailField(max_length=40)
-    phone = models.CharField(max_lenght=10)
+    phone = models.CharField(max_length=10)
 
 class Products(models.Model):
-    name = models.CharField(max_lenght=30)
-    section = models.CharField(max_lenght=30)
+    name = models.CharField(max_length=30)
+    section = models.CharField(max_length=30)
     price = models.IntegerField()
 
 class Orders(models.Model):
