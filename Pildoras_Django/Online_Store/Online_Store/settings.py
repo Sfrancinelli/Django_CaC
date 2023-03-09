94c0535b10671d10019a9f060cffd8e92acae193
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-03-09 15:34:06 -0300

    Started a new app (order_management) for e-commerce

--- Pildoras_Django/Online_Store/Online_Store/settings.py
+++ Pildoras_Django/Online_Store/Online_Store/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'order_management'
 ]
 
 MIDDLEWARE = [
--- Pildoras_Django/Online_Store/order_management/models.py
+++ Pildoras_Django/Online_Store/order_management/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Clients(models.Model):
+    name = models.CharField(max_lenght=30)
+    address = models.CharField(max_lenght=80)
+    email = models.EmailField(max_length=40)
+    phone = models.CharField(max_lenght=10)
+
+class Products(models.Model):
+    name = models.CharField(max_lenght=30)
+    section = models.CharField(max_lenght=30)
+    price = models.IntegerField()
+
+class Orders(models.Model):
+    order_id = models.IntegerField()
+    date = models.DateField()
+    delivered = models.BooleanField()
